31751b6085ee1e28b503bb46ef3f6ce037fc6380
Steven Armstrong <steven.armstrong@id.ethz.ch>
2017-03-21 07:04:20 +0100

    move examples into example-project
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- examples/setup.py
+++ examples/setup.py
@@ -1,31 +1,28 @@
 from setuptools import setup, find_packages
 
-name = 'chaos-cli'
+name = 'chaos-hello'
 
 setup(
     name=name,
-    version='0.1.0',
+    version='42',
     author='Steven Armstrong',
     author_email='steven-%s@armstrong.cc' % name,
-    description='Chaos cli tools and utilities',
+    description='Example to illustrate using chaos-cli',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX :: Linux',
     ],
-    #packages=find_packages(),
-    packages=['chaos.cli', 'chaos.cli.commands'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
         'click-plugins',
+        'chaos-cli',
     ],
     entry_points={
-        'console_scripts': [
-            'chaos = chaos.cli:main'
-        ],
         'chaos.cli.commands': [
-            'hello = chaos.cli.commands.hello:main',
+            'hello = chaos.hello.cli:main',
         ],
     },
 )
