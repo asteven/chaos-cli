08fcd2587f8532285e3bf2ebab789eb4b9cd3db8
Steven Armstrong <steven.armstrong@id.ethz.ch>
2017-03-22 08:20:09 +0100

    take alternative config file as option
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- chaos/cli.py
+++ chaos/cli.py
@@ -14,8 +14,10 @@ from click_plugins import with_plugins
 @click.group()
 @click.option('--verbose', '-v', 'log_level', flag_value='info', help='set log level to info', envvar='CHAOS_LOG_LEVEL')
 @click.option('--debug', '-d', 'log_level', flag_value='debug', help='set log level to debug', envvar='CHAOS_LOG_LEVEL')
+@click.option('--config', 'alternative_config', type=click.Path(readable=True, resolve_path=True),
+    help='Path to a alternative config file.')
 @click.pass_context
-def main(ctx, log_level):
+def main(ctx, log_level, alternative_config):
     """Bring order to your chaos.
     """
     setattr(ctx, 'obj', {})
@@ -27,17 +29,23 @@ def main(ctx, log_level):
         log.setLevel(getattr(logging, log_level.upper()))
     ctx.obj['log'] = log
 
-    # read config file if any
-    config_files = [
-        '/etc/%s/config.ini' % ctx.info_name,
-        '/usr/local/etc/%s/config.ini' % ctx.info_name,
-        os.path.join(click.get_app_dir(ctx.info_name), 'config.ini')
-    ]
+    # read config files if any
     config_file_exists = False
-    for config_file in config_files:
-        if os.path.isfile(config_file):
-            config_file_exists = True
-            log.debug('loading configuration from: %s', config_file)
+    if alternative_config:
+        config_files = [alternative_config]
+        # click already checked this for us
+        config_file_exists = True
+    else:
+        config_files = [
+            '/etc/%s/config.ini' % ctx.info_name,
+            '/usr/local/etc/%s/config.ini' % ctx.info_name,
+            os.path.join(click.get_app_dir(ctx.info_name), 'config.ini')
+        ]
+        for config_file in config_files:
+            if os.path.isfile(config_file):
+                config_file_exists = True
+                log.debug('loading configuration from: %s', config_file)
+
     config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
     if config_file_exists:
         config.read(config_files)
