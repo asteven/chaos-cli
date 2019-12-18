3151d58ed13dc18bed715c2af63bfbed0a4392e6
Steven Armstrong <steven.armstrong@id.ethz.ch>
2019-12-18 15:56:40 +0100

    cleanup based on clusterctl
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- chaos/cli.py
+++ chaos/cli.py
@@ -10,6 +10,19 @@ from click_plugins import with_plugins
 import reentry.manager
 
 
+def get_config_files(ctx, file_name):
+    possible_config_files = [
+        '/etc/%s/%s' % (ctx.info_name, file_name),
+        '/usr/local/etc/%s/%s' % (ctx.info_name, file_name),
+        os.path.join(click.get_app_dir(ctx.info_name), file_name),
+    ]
+    config_files = []
+    for config_file in possible_config_files:
+        if os.path.isfile(config_file):
+            config_files.append(config_file)
+    return config_files
+
+
 @with_plugins(reentry.manager.iter_entry_points('chaos.cli.commands'))
 @click.group()
 @click.option('--verbose', '-v', 'log_level', flag_value='info', help='set log level to info', envvar='CHAOS_LOG_LEVEL')
@@ -22,40 +35,30 @@ def main(ctx, log_level, alternative_config):
     """
     setattr(ctx, 'obj', {})
 
-    # configure logger and store it in the context
-    logging.basicConfig(level=logging.ERROR, format='%(levelname)s: %(message)s', stream=sys.stderr)
+    # Configure logging.
+    logging_config_files = get_config_files(ctx, 'logging.ini')
+    if logging_config_files:
+        logging_config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+        logging_config.read(logging_config_files)
+        logging.config.fileConfig(logging_config)
+    else:
+        logging.basicConfig(level=logging.ERROR, format='%(levelname)s: %(message)s', stream=sys.stderr)
     log = logging.getLogger('chaos')
+    ctx.obj['log_level'] = log_level
     if log_level:
         log.setLevel(getattr(logging, log_level.upper()))
     ctx.obj['log'] = log
 
-    # read config files if any
-    config_file_exists = False
-    if alternative_config:
-        config_files = [alternative_config]
-        # click already checked this for us
-        config_file_exists = True
-    else:
-        config_files = [
-            '/etc/%s/config.ini' % ctx.info_name,
-            '/usr/local/etc/%s/config.ini' % ctx.info_name,
-            os.path.join(click.get_app_dir(ctx.info_name), 'config.ini')
-        ]
-        for config_file in config_files:
-            if os.path.isfile(config_file):
-                config_file_exists = True
-                log.debug('loading configuration from: %s', config_file)
 
+    # Configure application.
     config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
-    if config_file_exists:
+    config_files = get_config_files(ctx, 'config.ini')
+    if config_files:
+        log.debug('loading configuration from: %s', ', '.join(config_files))
         config.read(config_files)
     else:
-        # TODO: error out no config file was found
-        # hardcode defaults for now
-        log.debug('No configuration file found, falling back to defaults.')
-        _config = collections.OrderedDict()
-        _config['consul'] = {'url': 'http://localhost:8500'}
-        config.read_dict(_config)
+        log.warn('No configuration file found.')
+        #raise exceptions.Error('No configuration file found.')
     ctx.obj['config'] = config
 
 
